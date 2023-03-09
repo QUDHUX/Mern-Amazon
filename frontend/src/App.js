5903be4cea964c855ae53d6894d38b8b7d5fdea3
Qudus Oshinaike <llahsmine@gmail.com>
2023-03-10 07:57:36 +0800

    Create-signin-screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { Store } from './store';
 import { useContext } from 'react';
 import CartScreen from './Screens/CartScreen';
+import SigninScreen from './Screens/SigninScreen';
 
 function App() {
   const {state} = useContext(Store)
@@ -42,6 +43,7 @@ function App() {
               <Route path="/" element={<HomeScreen />} />
               <Route path="/products/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
