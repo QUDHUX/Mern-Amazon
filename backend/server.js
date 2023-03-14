b91f42ea98a8424f8e341868f0014cc7eaa4b2b3
Qudus Oshinaike <llahsmine@gmail.com>
2023-03-15 06:23:14 +0800

    Create-signin-backend-API

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,33 @@
 import express from 'express';
-import data from './data.js';
+//import data from './data.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
 import seedRouter from './Routes/seedRoute.js';
 import productRouter from './Routes/productRoute.js';
-
+import userRouter from './Routes/userRoute.js';
 
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI).then(()=>{
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
     console.log('connected to db');
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err.message);
-});
+  });
 
 const app = express();
 
-app.use('/api/seed', seedRouter)
-app.use('/api/products', productRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
