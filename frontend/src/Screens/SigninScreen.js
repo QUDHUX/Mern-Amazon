895c35446451abb098cd7c7ae0fa55ceb170f4c1
Qudus Oshinaike <llahsmine@gmail.com>
2023-03-16 07:59:23 +0800

    Complete-signin-screen

--- frontend/src/Screens/SigninScreen.js
+++ frontend/src/Screens/SigninScreen.js
@@ -1,38 +1,75 @@
-import Button from "react-bootstrap/esm/Button";
-import Form from "react-bootstrap/esm/Form";
-import Container from "react-bootstrap/esm/Container";
-import { Link, useLocation } from "react-router-dom";
-import {Helmet} from "react-helmet-async";
+import Button from 'react-bootstrap/esm/Button';
+import Form from 'react-bootstrap/esm/Form';
+import Container from 'react-bootstrap/esm/Container';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+import { Store } from '../store';
+import { toast } from 'react-toastify';
+import { getError } from '../util';
 
-export default function SigninScreen(){
-    const {search} = useLocation();
-    const redirectInUrl = new URLSearchParams(search).get('redirect');
-    const redirect = redirectInUrl ? redirectInUrl : '/';
+export default function SigninScreen() {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
 
-   
-    return(
-        <Container className="small-container">
-            <Helmet>
-                <title>Sign In</title>
-            </Helmet>
-            <h1 className="my-3">Sign In</h1>
-            <Form>
-                <Form.Group className="mb-3" controlid="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlid="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" required />
-                </Form.Group>
-                <div className="mb-3">
-                    <Button type="submit">Sign In</Button>
-                </div>
-                <div className="mb-3">
-                    New customer{''}
-                    <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
-                </div>
-            </Form>
-        </Container>
-    )
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/signin', {
+        email,
+        password,
+      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    } catch(err) {
+      toast.error(getError(err));
+    }
+  };
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      <h1 className="my-3">Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlid="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlid="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
+        </div>
+        <div className="mb-3">
+          New customer{''}
+          <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
 }
